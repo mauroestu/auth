0f8b95716e240be347481c063d25322fa38d41d1
mauroestu <mauroestu18@gmail.com>
2019-05-10 15:54:29 -0600

    holaaaa

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	port: process.env.PORT || 8000,
+	port: process.env.PORT || 8001,
 	secret: 'ADSfqefadfawdFAEFA'
 }
--- controllers/connect.js
+++ controllers/connect.js
@@ -3,9 +3,9 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : '35.225.155.247',
     user     : 'root',
-    password : '',
+    password : '123',
     database : 'auth'
 });
 
